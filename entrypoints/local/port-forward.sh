cdf27eac50f170ab004efbb8510d429b1ef397cc
Benjamin Zhang <benjamin@biteinvestments.com>
2022-03-09 22:59:52 +0800

    feat: 🎸 add local for port forward

--- entrypoints/local/port-forward.sh
+++ entrypoints/local/port-forward.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-az login -u "${AZURE_USERNAME}" -p "${AZURE_PASSWORD}"
 az aks get-credentials --resource-group "${AZURE_RESOURCE_GROUP}" --name "${AZURE_AKS_NAME}"
 kubectl port-forward "${POD_NAME}" --namespace "${POD_NAMESPACE}" "${LOCAL_PORT}":"${POD_PORT}" --address='0.0.0.0'
